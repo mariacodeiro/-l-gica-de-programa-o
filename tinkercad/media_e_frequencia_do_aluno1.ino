a660d2b45a961ec06e7363628efb26973e4911bb
Maria Eduarda <dudinhacordeiro2008@gmail.com>
2025-05-14 10:12:27 -0300

    tinkercad

---
+++ tinkercad/media_e_frequencia_do_aluno1.ino
@@ -0,0 +1,42 @@
+// C++ code
+//variaveis 
+
+float nota1, nota2,frequencia;
+
+void setup();
+}
+  Serial.begin(9600);
+} 
+  Serial.println("digite nota1");
+  Serial.println();
+  while( !Serial.available());
+  nota = Serial.parseInt();
+
+  Serial.println("digite nota2");
+  Serial.println();
+  while( !Serial.available());
+  nota = Serial.parseInt();
+
+  Serial.println("digite sua frequencia");
+  Serial.println();
+  while( !Serial.available());
+  nota = Serial.parseInt();
+  
+}if(nota>= 0 && nota <= 5){//peguei do outro exercicio, pois tinha esquecido
+  Serial.println("reprovado");
+    
+} else if(nota >= 5 && nota < 10); {
+  Serial.println("aprovado");
+    
+} else if (nota >= 0% && nota < 50%);{
+  Serial.println("reprovado");
+    
+} else if(nota >= 50% && idade < 100%);{
+  Serial.println("aprovado");
+    
+ delay(1000);
+
+void loop()
+{
+  
+}
